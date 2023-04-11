3a862e5c453933fa9d461a197688554b0b42babc
Kashafz <129360649+Kashafz@users.noreply.github.com>
2023-04-11 21:27:39 +0500

    Add assognment 2 saylani

--- app.js
+++ app.js
@@ -1,87 +1,29 @@
-//question no 1:
-// let num1=prompt("Enter number 1");
-// let num2=prompt("Enter number 2");
+//Question 1
 
-// num1 = parseInt(num1);
-// num2 = parseInt(num2);
-
-// if(num1 > num2){
-//     console.log("num 1 is largest number")
-// }
-// else{
-//     console.log("num 2 is largest number")
-
-// }
-
-//question 2
-
-numberCheck=prompt("Enter a number");
-if(numberCheck > 0){
-    alert("The number is positive");
+function add() {
+    let five = 5;
+    return function(num){
+        console.log(five + num)
+    }  
 }
-  else{
-    alert("The number is negative")
-  }
-
-  //question 3
-
-//   let Num1=prompt("Enter number 1");
-//   let Num2=prompt("Enter number 2");
-//   let Num3=prompt("Enter number 3");
-//   let Num4=prompt("Enter number 4");
-//   let Num5=prompt("Enter number 5");
+let passValue = add();
+passValue(20);
 
 
-//   if(Num1 > Num2 && Num1 > Num3 && Num1 > Num4 && Num1 > Num5){
-//     console.log(Num1 +" is largest number")
-//   } else if(Num2 > Num1 && Num2 > Num3 && Num2 > Num4 && Num2 > Num5){
-//     console.log(Num2 +" is largest number")
-//   }
-//     else if(Num3 > Num1 && Num3 > Num2 && Num2 > Num4 && Num2 > Num5){
-//         console.log(Num3 +" is largest number");
-    
-//   } else if(Num4 > Num1 && Num4 > Num2 && Num4 > Num3 && Num4 > Num5){
-//     console.log(Num4 +" is largest number");
-// }
-// else{
-//     console.log(Num5 +" is largest number");
+// Question 3
 
-// }
-
-for(var i=0; i<=15; i++ ){
-    if(i % 2==0){
-        console.log(i +" is even")
-    }
-    else{
-        console.log(i + "is odd")
-    }
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Kashaf";
 }
+myFunction()
 
+// Question 4
 
-//question 6
-for(var i=1; i<=100; i++){
-
-    if(i %3==0 && i %5==0){
-        console.log(i +"FizzBuzz");
-    }
-    else if(i %3==0){
-        console.log(i +"Fizz")
-
-    }else if(i %5==0){
-        console.log(i +"Buzz")
-
-    }
-    else{
-        console.log(i)
-    }
+var unList = document.getElementById("ul");
+function list(item) {
+    var li_1 = document.createElement("li")
+        li_1.innerHTML = item
+        unList.appendChild(li_1);
 }
-
-//question no7
-
-for(let i =1 ; i<=5; i++){
-    let row='';
-    for(let j=1; j<=i; j++){
-        row +="*"
-    }
-    console.log(row)
-}
+list("123");
+list("Kashaf Ali");
